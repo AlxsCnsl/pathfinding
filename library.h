2de069f0c05f9112173b16ed1bb838fb73544fb8
Consolo ALexis <133843787+Piraweed@users.noreply.github.com>
2024-10-18 14:04:07 +0200

    0.0.9
    add Error(Enum)

--- library.h
+++ library.h
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum{
+    FILE_NOT_FOUND = 1,
+    NO_START_NODE = 2,
+    NO_END_NODE = 3,
+    NO_VALIDE_PATH = 4,
+    BAD_FILE_FORMAT = 5
+} Error;
+
 typedef struct n{
     int id;
     truct n **links;
