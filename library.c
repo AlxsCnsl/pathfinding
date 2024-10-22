ac51b87be3805116b8d4f970ae171adb2ca8786a
Consolo ALexis <133843787+Piraweed@users.noreply.github.com>
2024-10-22 21:20:55 +0200

    Alexis[12]

--- library.c
+++ library.c
@@ -59,7 +59,7 @@ int get_links_nbr(char *filename)
     str_debuts_str("#links", buffer)){}//while
     while (fgets(buffer, sizeof(buffer), file) != NULL)
     {
-        links_nbr++;
+        links_nbr ++;
     }
     return links_nbr;
 }
@@ -105,25 +105,81 @@ int get_node_id_by_index(char *filename, int index){
 }
 
 
-int get_nbr_links_by_id()
+int get_links_nbr_by_id(int id, char* filename) //retourne le nombre de lien qu'à un node
 {
-
+    FILE *file = fopen(filename, "r");
+    char buffer[256];
+    int i, left_id, right_id, nods_nbr = get_links_nbr(filename);
+    int retun_value = 0;
+    while (fgets(buffer, sizeof(buffer), file)!= NULL &&//while le while pour ignorer tout jusqu'a #links
+    str_debuts_str("#links", buffer)){}//while
+    for (i=1; i<nods_nbr; i++){
+        if(fgets(buffer, sizeof(buffer), file)!= NULL && //if//    on incremente le curseur de file et on aplique la ligne a bufer buffer 
+        sscanf(buffer, "%d-%d", &left_id, &right_id) == 2) //if/   et on change les valeurs de  left_id et right_id
+        printf(">%d:%d\n", left_id, right_id);
+        {
+            if(left_id == id || right_id == id)
+            {
+                printf(">>>>%d:%d pour index %d\n", left_id, right_id, id);
+                retun_value ++;
+            }
+        }
+    }
+    fclose(file); 
+    return retun_value;
 }
 
-
-void init_links_node(Node* node,)
+int get_oposit_id_in_file(int id, char* filename, int rank)//ex: si id = 1 & rank =2 et que fille =  1-2\n1-3 on a return 3; car c'est le 2e fois qu'on vois 1;
 {
-    int size =  
+    FILE *file = fopen(filename, "r");
+    char buffer[256];
+    int i, left_id, right_id, nods_nbr = get_nods_nbr(filename);
+    int rank_lvl = 1,retun_value = 0;
+    while (fgets(buffer, sizeof(buffer), file)!= NULL &&//while le while pour ignorer tout jusqu'a #links
+    str_debuts_str("#links", buffer)){}//while
+    for (i=0; i<nods_nbr; i++){
+        if(fgets(buffer, sizeof(buffer), file)!= NULL && //if//    on incremente le curseur de file et on aplique la ligne a bufer buffer 
+        sscanf(buffer, "%d-%d", &left_id, &right_id) == 2) //if/   et on change les valeurs de  left_id et right_id
+        {
+            if((left_id==id && right_id!=id)||(left_id!=id && right_id==id)) //un if pour regarder si il y à id dans l'in des 2 coté 
+            {        
+                if(rank_lvl == rank)
+                {
+                    if(left_id == id && right_id != id)
+                    {
+                        return right_id;
+                    }
+                    return left_id;
+                }
+                rank_lvl ++;
+            }
+        }
+    }
+    fclose(file); 
+    return 0;
 }
+
+void init_links_node(Node* node, char* filename) //A finir
+{
+    int i, size = get_links_nbr_by_id(node->id, filename);
+    node->links = (Node**)malloc(size*sizeof(Node**));
+    for(i = 0; i<size; i++)
+    {
+        //links[i] =
+    }
+}
+
 
 Node** init_node( char *filename )
 {
     int i, nodes_nbr = get_nods_nbr(filename);
     Node** n_tab = (Node**)malloc(nodes_nbr * sizeof(Node*));
     
-    for(i=0;i<nodes_nbr;i++){
+    for(i=0;i<nodes_nbr;i++)
+    {
         n_tab[i] = (Node*)malloc(sizeof(Node));
         n_tab[i]->id = get_node_id_by_index(filename, i);
+        init_links_node(n_tab[i], filename);
     }
     return n_tab;
 }
@@ -132,10 +188,13 @@ Node** init_node( char *filename )
 Node* get_node_by_id(Node **nodes, int size, int id)
 {
     int i;
-    for(i=0; i<size; i++){
-        printf("ON PASSE dans le noeud à id %d à index %d\n",nodes[i]->id, i );
-        if(id == nodes[i]->id){
+    for(i=0; i<size; i++)
+    {
+        printf("ON PASSE dans le noeud à id %d à index %d, et la size = %d\n",nodes[i]->id, i, size);
+        if(id == nodes[i]->id)
+        {
             return nodes[i];
         }
     }
+    return NULL;
 }
--- main.c
+++ main.c
@@ -14,8 +14,12 @@ int main(int argc, char* argv[])
     display_node_end(argv[1]);
     Node **nodes_array = init_node(argv[1]);
 
-    Node* nd = get_node_by_id(nodes_array,get_nods_nbr(argv[1]), 14);
+    Node* nd = get_node_by_id(nodes_array, get_nods_nbr(argv[1]), 2);
     printf("id de nd : %d\n", nd->id);
+    int nbr_s = get_links_nbr_by_id(nd->id, argv[1]);
+    printf("Size = %d\n",nbr_s );
+    int lol = get_oposit_id_in_file(4, argv[1], 3);
+    printf("Oposit = %d", lol);
     
     //init_node(argv[1]);
 }
